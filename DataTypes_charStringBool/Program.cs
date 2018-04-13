8e0cd606b028837475d4f50eb607d69f4414139e
To Be Or Not to be <seniory@windowslive.com>
2018-04-13 18:44:01 +0300

    Added alot of variables

--- DataTypes_charStringBool/Program.cs
+++ DataTypes_charStringBool/Program.cs
@@ -10,6 +10,26 @@ namespace DataTypes_charStringBool
     {
         static void Main(string[] args)
         {
+            int CharMin = Char.MinValue;
+            int CharMax = char.MaxValue;
+
+            //تصريح عن متغير وتحويل قيمته لرقم
+            char MyVarvalue2 = Convert.ToChar(90);
+            //متغير يأخذ قيمة المتغيرالمحوله إلى أحرف ويحولها إلى قيمتها الرقمية
+            int VarValue = MyVarvalue2;
+            Boolean TrueFalse = true;
+            Console.WriteLine("\nMin Char = {0} \nMax Char = {1}\nMy Variable Number={2} \nMy Conversion Value is = {3}",
+                CharMin, CharMax, VarValue ,MyVarvalue2);
+            Console.ReadKey();
+            Console.WriteLine("Is it True what I've heared.......?     {0}", TrueFalse);
+            Console.ReadKey();
+            String myString = "This is my string";
+            Console.WriteLine("\nMy String is {0}", myString);
+            Console.ReadKey();
+
+
+
+
         }
     }
 }
